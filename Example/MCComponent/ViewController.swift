6cce0eb52521f1411d8c620e98e8861a72f0b9e3
AlexKostiev <akostiev@griddynamics.com>
2018-10-09 17:44:40 +0300

    Initial commit

---
+++ Example/MCComponent/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MCComponent
+//
+//  Created by AlexKostiev on 10/09/2018.
+//  Copyright (c) 2018 AlexKostiev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
